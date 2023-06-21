8aa6b1e3cbbf33a86654add2c7b4883709a3faef
Laurent Ouma <lawiomosh3@gmail.com>
2023-06-21 08:07:00 +0300

    Importing from excell however so mannual

--- core/management/commands/import_excel.py
+++ core/management/commands/import_excel.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand
 from openpyxl import load_workbook
 from core.models import Students
+
+
 class Command(BaseCommand):
     help = 'Import Excel data into the database'
     headers = []
@@ -12,27 +14,18 @@ class Command(BaseCommand):
         file_path = options['file_path']
         workbook = load_workbook(filename=file_path)
         sheet = workbook.active
-
-        # Loop through the DataFrame rows and save them as model instances
         for index, row in enumerate(sheet.rows):
-            # my_model = MyModel(
-            #     # Assign column values to model fields
-            #     field1=row['Column1'],
-            #     field2=row['Column2'],
-            #     # Add more fields as necessary
-            # )
-            # my_model.save()
-            # self.stdout.write(self.style.SUCCESS(f'Successfully imported row {index + 1}'))\
             # Get headers
             if index == 0:
-                self.headers = [(cell.value).lower().replace(" ", "_") for cell in row]
+                self.headers = [str(cell.value).lower().replace(" ", "_") for cell in row]
             # get values
             else:
                 values = [cell.value if cell.value else "" for cell in row]
-                kwargs = dict(zip(self.headers, values))
-                print(kwargs)
-                return
-                # Students.objects.create(**kwargs)
-                # self.stdout.write(self.style.SUCCESS(f'Successfully imported row {index}'))
+                # kwargs = dict(zip(self.headers, values))
+                student = Students()
+                for key, value in zip(self.headers, values):
+                    setattr(student, key, value)
+                student.save()
+                self.stdout.write(self.style.SUCCESS(f'\rSuccessfully imported row {index}'), ending='')
 
         self.stdout.write(self.style.SUCCESS('Data import completed.'))
--- core/models.py
+++ core/models.py
@@ -9,11 +9,11 @@ class Students(models.Model):
     gender = models.CharField(max_length=50)
     id_number = models.CharField(max_length=50)
     centre_code = models.CharField(max_length=50)
-    centre_name = models.CharField(max_length=50), 
-    grade = models.CharField(max_length=50),
-    trade_code = models.CharField(max_length=50), 
-    trade_name = models.CharField(max_length=50), 
-    test_series = models.CharField(max_length=50), 
-    result = models.CharField(max_length=50), 
+    centre_name = models.CharField(max_length=50)
+    grade = models.CharField(max_length=50)
+    trade_code = models.CharField(max_length=50)
+    trade_name = models.CharField(max_length=50)
+    test_series = models.CharField(max_length=50)
+    result = models.CharField(max_length=50)
     photo_link = models.CharField(max_length=50)
     
